d365ff94ab1e2bf2ff570e2630f9e83bf69aff44
Anvay Bhanap <anvay.bhanap@gmail.com>
2020-12-06 03:10:01 -0800

    Update Main.java to include Leaderboard window

--- src/main/java/application/Main.java
+++ src/main/java/application/Main.java
@@ -1,11 +1,10 @@
 package application;
 
-import application.Tutorial;
 import javafx.application.Application;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
-import javafx.stage.Stage;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
@@ -13,12 +12,11 @@ import javafx.scene.effect.InnerShadow;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.Pane;
-import javafx.scene.layout.StackPane;
 import javafx.scene.paint.Color;
 import javafx.scene.text.Font;
 import javafx.scene.text.FontWeight;
 import javafx.scene.text.Text;
-import javafx.scene.Parent;
+import javafx.stage.Stage;
 
 public class Main extends Application {
 
@@ -67,35 +65,52 @@ public class Main extends Application {
 
 		});
 
-		tutorial = new Button("Tutorial");
-		tutorial.relocate(355, 500);
-
-		tutorial.setOnAction(e -> {
-			Tutorial.display("Tutorial", "Welcome to Brick-Breaker!" + "\n" + "\nHere’s how to play the game:" + "\n"
-					+ "\nYour goal is to knock out all the bricks on the screen by moving the paddle. "
-					+ "\n\nLaunch the ball by pressing the SPACE bar."
-					+ "\nControl the paddle using the LEFT and RIGHT arrow keys on your keyboard." + "\n"
-					+ "\nDon't let the ball hit the bottom of the screen, otherwise you lose a life."
-					+ "\nYou have 5 lives." + "\n\nHave fun!! :)");
-
-		});
-
 		rankings = new Button("Leaderboard");
 		rankings.relocate(340, 550);
+		
+		
+		
+		rankings.setOnAction(new EventHandler<ActionEvent>() {
 
-		closeButton = new Button("Close game");
-		closeButton.setOnAction(e -> window.close());
-		closeButton.relocate(343, 600);
+			@Override
+			public void handle(ActionEvent event) {
+				Parent root = null;
 
-		Image img = new Image("file:back.jpeg");
-		ImageView mv = new ImageView(img);
+				try {
+					root = FXMLLoader.load(getClass().getResource("/table.fxml"));
+				} catch (java.io.IOException e) {}
 
-		Pane layout = new Pane();
-		layout.getChildren().addAll(mv, title, play, tutorial, rankings, closeButton);
-		Scene scene = new Scene(layout, 800, 800);
-		window.setScene(scene);
-		window.setResizable(false);
-		window.show();
+				Scene scene = new Scene(root, 600, 400);
+				Game game = new BrickBreaker(primaryStage, scene);
+				primaryStage.setTitle(game.getTitle());
+				primaryStage.setResizable(false);
+
+				primaryStage.show();
+				
+			}
+
+        });
+
+	tutorial=new Button("Tutorial");tutorial.relocate(355,500);
+
+	tutorial.setOnAction(e -> {Tutorial.display("Tutorial","Welcome to Brick-Breaker!"+
+			"\n"+"\nHere’s how to play the game:"+
+			"\n"+"\nYour goal is to knock out all the bricks on the screen by moving the paddle. "+
+			"\n\nLaunch the ball by pressing the SPACE bar."+
+			"\nControl the paddle using the LEFT and RIGHT arrow keys on your keyboard."+"\n"+
+			"\nDon't let the ball hit the bottom of the screen, otherwise you lose a life."+
+			"\nYou have 5 lives."+
+			"\n\nHave fun!! :)");
+
+	});
+
+	closeButton=new Button("Close game");closeButton.setOnAction(e->window.close());closeButton.relocate(343,600);
+
+	Image img = new Image("file:back.jpeg");
+	ImageView mv = new ImageView(img);
+
+	Pane layout = new Pane();layout.getChildren().addAll(mv,title,play,tutorial,rankings,closeButton);
+	Scene scene = new Scene(layout, 800, 800);window.setScene(scene);window.setResizable(false);window.show();
 
 	}
 
